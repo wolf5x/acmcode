e96331571fee8cce8290d761a9091980122f064e
wolf5x <wolf5x1016@gmail.com>
2013-01-13 17:00:13 +0800

    srm566 250 500

---
+++ topcoder/srm566/PenguinEmperor.cpp
@@ -0,0 +1,113 @@
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <list>
+#include <stdexcept>
+#include <functional>
+#include <utility>
+#include <ctime>
+using namespace std;
+
+#define PB push_back
+#define MP(x,y) make_pair((x),(y))
+#define FI(x) ((x).first)
+#define SE(x) ((x).second)
+#define SZ(x) ((x).size())
+#define LEN(x) ((x).length())
+
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,l,h) for(int i=(l);i<=(h);++i)
+#define FORD(i,h,l) for(int i=(h);i>=(l);--i)
+#define FORIT(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define FORRIT(i,c) for(typeof((c).rbegin()) i=(c).rbegin(); i!=(c).rend(); ++i)
+
+#define CLR(x) memset((x),0,sizeof(x))
+#define SET(x,y) memset((x),(y),sizeof(x))
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<double> VD;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+const int MOD = 1000000007;
+int n;
+
+void multi(int x[], int y[], int r[]) {
+	int t[355] = {};
+	REP(j,n) if(y[j]) REP(i, n) if(x[(n-j+i)%n]) {
+		t[i] = (t[i] + (long long)x[(n-j+i)%n]*y[j]) %MOD;
+	}
+	memcpy(r, t, sizeof(t));
+}
+
+void mypow(int x[], long long e, int r[]) {
+	int y[355] = {0}, t[355] = {1};
+	memcpy(y, x, sizeof(y));
+	while(e) {
+		if(e%2) multi(t, y, t);
+		multi(y, y, y);
+		e >>= 1;
+	}
+	memcpy(r, t, sizeof(t));
+}
+
+class PenguinEmperor {
+    public:
+    int countJourneys(int numCities, long long daysPassed) {
+		n = numCities;
+		int r[355] = {1}, a[355] = {1}, b[355] = {1};
+		FOR(i,1,n) {
+			int d[355] = {0};
+			d[i] = d[n-i] = 1;
+			multi(b, d, b);
+			if(i == daysPassed%n)
+				memcpy(a, b, sizeof(b));
+		}
+		mypow(b, daysPassed/n, b);
+		multi(r, a, r);
+		multi(r, b, r);
+		return r[0];
+    }
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); if ((Case == -1) || (Case == 6)) test_case_6(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arg0 = 3; long long Arg1 = 2LL; int Arg2 = 2; verify_case(0, Arg2, countJourneys(Arg0, Arg1)); }
+	void test_case_1() { int Arg0 = 4; long long Arg1 = 3LL; int Arg2 = 2; verify_case(1, Arg2, countJourneys(Arg0, Arg1)); }
+	void test_case_2() { int Arg0 = 5; long long Arg1 = 36LL; int Arg2 = 107374182; verify_case(2, Arg2, countJourneys(Arg0, Arg1)); }
+	void test_case_3() { int Arg0 = 300; long long Arg1 = 751LL; int Arg2 = 413521250; verify_case(3, Arg2, countJourneys(Arg0, Arg1)); }
+	void test_case_4() { int Arg0 = 300; long long Arg1 = 750LL; int Arg2 = 0; verify_case(4, Arg2, countJourneys(Arg0, Arg1)); }
+	void test_case_5() { int Arg0 = 350; long long Arg1 = 1000000000000000000LL; int Arg2 = 667009739; verify_case(5, Arg2, countJourneys(Arg0, Arg1)); }
+	void test_case_6() { int Arg0 = 5; long long Arg1 = 7LL; int Arg2 = 12; verify_case(6, Arg2, countJourneys(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    PenguinEmperor ___test;
+    ___test.run_test(-1);
+    return 0;
+}
+// END CUT HERE
---
+++ topcoder/srm566/PenguinSledding.cpp
@@ -0,0 +1,86 @@
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+using namespace std;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define CLR(x) memset((x),0,sizeof(x))
+#define SET(x,y) memset((x),(y),sizeof(x))
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<double> VD;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+class PenguinSledding {
+	int d[55], w[55][55];
+    public:
+    long long countDesigns(int numCheckpoints, vector <int> checkpoint1, vector <int> checkpoint2) {
+		memset(d, 0, sizeof(d));
+		memset(w, 0, sizeof(w));
+		long long ans = 0;
+		int n = checkpoint1.size();
+		ans = n+1;	
+		REP(i, n) {
+			d[checkpoint1[i]-1]++, d[checkpoint2[i]-1]++;
+			w[checkpoint1[i]-1][checkpoint2[i]-1] = w[checkpoint2[i]-1][checkpoint1[i]-1] = 1;
+		}
+		REP(i, 55) {
+			if(d[i] >= 2) ans += (1LL<<d[i])-d[i]-1;
+		}
+		REP(i, 55) for(int j = i+1; j < 55; j++) for(int k = j+1; k < 55; k++)
+			if(w[i][j] && w[j][k] && w[k][i]) ans++;
+		return ans;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arg0 = 2; int Arr1[] = {1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 2LL; verify_case(0, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arg0 = 4; int Arr1[] = {1,2,3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,3,4}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 6LL; verify_case(1, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arg0 = 6; int Arr1[] = {1,3,6}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,4,4}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 5LL; verify_case(2, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_3() { int Arg0 = 50; int Arr1[] = {40, 40, 40, 40, 40, 40, 40, 40, 
+ 40, 40, 40, 40, 50, 40, 40, 40, 
+ 23, 4, 24, 40, 22, 40, 8, 40, 40,
+ 40, 34, 21, 40, 40, 38, 40, 40, 
+ 40, 40, 40, 28, 40, 40, 40, 40, 
+ 46, 13, 40, 40, 40, 47, 40, 40}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {45, 42, 20, 48, 12, 32, 25, 10, 
+ 26, 39, 16, 2, 19, 43, 37, 17, 
+ 19, 19, 19, 18, 19, 27, 19, 29, 
+ 11, 36, 19, 19, 1, 41, 19, 35, 
+ 14, 33, 49, 3, 19, 7, 5, 19, 31, 
+ 19, 19, 6, 9, 15, 19, 44, 30}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 68719493118LL; verify_case(3, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_4() { int Arg0 = 36; int Arr1[] = {13, 24, 24, 20, 31, 16, 10, 36, 34, 32, 
+ 28, 5, 20, 29, 23, 2, 14, 4, 9, 5, 19, 
+ 21, 8, 1, 26, 27, 25, 15, 22, 30, 30, 
+ 6, 11, 7, 2, 35, 13, 29, 4, 12, 33, 18, 
+ 13, 14, 17, 35, 3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {10, 15, 27, 1, 29, 11, 5, 18, 33, 1, 9,
+ 2, 31, 6, 19, 10, 33, 18, 6, 27, 3, 22,
+ 4, 12, 31, 30, 34, 16, 7, 24, 3, 28, 15,
+ 21, 22, 8, 26, 20, 14, 32, 25, 17, 35,
+ 8, 36, 26, 23}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 162LL; verify_case(4, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    PenguinSledding ___test;
+    ___test.run_test(-1);
+    return 0;
+}
+// END CUT HERE
